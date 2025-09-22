ba0a2459e2dcbc84e51db863efb0800162dc6ac8
Khayree11 <Irfadbindhony@gmail.com>
2025-09-22 22:13:34 +0700

    feat: adding user migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,12 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id('id_user');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password_user');
             $table->timestamps();
         });
     }

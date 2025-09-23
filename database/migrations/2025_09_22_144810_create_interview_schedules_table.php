cb30c77552263ad1b2c386acdb79e68fe67f6443
MUHAMMADFIRDASATRIA <satriajjn@students.unnes.ac.id>
2025-09-23 13:02:45 +0700

    fix: Correct table name in down migration for interview schedules

--- database/migrations/2025_09_22_144810_create_interview_schedules_table.php
+++ database/migrations/2025_09_22_144810_create_interview_schedules_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('interview_scheduless');
+        Schema::dropIfExists('interview_schedules');
     }
 };
